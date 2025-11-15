025a6e97f33715631039ecd56bd585b273fbf048
Nolan Hill <nolanhill-developer@pm.me>
2025-11-15 09:50:20 -0700

    Initial project and creation of part 1 - print information of fictional character.

---
+++ CT1/src/CT1.cpp
@@ -0,0 +1,26 @@
+//============================================================================
+// Name        : CT1.cpp
+// Author      : Nolan Hill
+// Version     : 1
+// Copyright   : Your copyright notice
+// Description : Simple program to print information of fictional character
+//============================================================================
+
+#include <iostream>
+
+// Standard namespace declaration
+using namespace std;
+
+// Main function
+int main() {
+
+	// Print fictional character's information to console
+	cout << "First Name: John" << endl; 			// Print first name and end with newline
+	cout << "Last Name: Smith" << endl; 			// Print last name and end with newline
+	cout << "Address: 123 Main Street" << endl; 	// Print address and end with newline
+	cout << "City: Phoenix" << endl; 				// Print city and end with newline
+	cout << "Zip Code: 85001" << endl; 				// Print zip code and end with newline
+
+	// Return integer, indicating no error
+	return 0;
+}
