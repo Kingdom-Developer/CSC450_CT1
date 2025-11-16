84419a71d1a9351ba5b20517484f19350ba5e685
Nolan Hill <nolanhill-developer@pm.me>
2025-11-16 11:58:19 -0700

    Separated the three parts of assigment into its own project for clarity.

---
+++ Part1/src/Part1.cpp
@@ -0,0 +1,22 @@
+//============================================================================
+// Name        : Part1.cpp
+// Author      : Nolan
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	// Print fictional character's information to console
+	cout << "First Name: John" << endl; 			// Print first name and end with newline
+	cout << "Last Name: Smith" << endl; 			// Print last name and end with newline
+	cout << "Address: 123 Main Street" << endl; 	// Print address and end with newline
+	cout << "City: Phoenix" << endl; 				// Print city and end with newline
+	cout << "Zip Code: 85001" << endl; 				// Print zip code and end with newline
+
+	// Return integer, indicating no error
+	return 0;
+}
